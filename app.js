2387a612fd7b8a08f13cdf3d80fb414794ca1d43
john6myers <john6myers@gmail.com>
2022-07-27 21:38:28 -0400

    let and const exervise

---
+++ app.js
@@ -0,0 +1,12 @@
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+// ES2015
+const PI = 3.14;
+PI = 42; // <-- will give an error now
+
+// Quiz answers
+// 1. You can reassign and redeclare with var but you can’t redeclare using let 
+// 2. You can reassign and redeclare with var but you can’t redeclare using const 
+// 3. You can reassign with let but you can’t redeclare or reassign using const
+// 4 .The process where the declaration is moved to the top. It allows functions to be safely used in code before they are declared 
